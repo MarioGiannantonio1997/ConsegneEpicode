f657d8afb84293098ecd17edfdb43d9c93cb9a91
Uro <93337886+MarioGiannantonio1997@users.noreply.github.com>
2025-04-10 19:28:42 +0200

    Add files via upload

---
+++ W1D1.java
@@ -0,0 +1,39 @@
+
+Attributi del Personaggio
+
+Strenght (STR) – Determina il danno fisico e la capacità di impugnare armi pesanti.
+Agility (AGI) – Incide sulla velocità di turno, la schivata e i colpi critici.
+Resistence (RES) – Influenza i punti vita e la riduzione del danno ricevuto.
+Intelligence (INT) – Potenzia l’efficacia delle abilità speciali e la rigenerazione di energia.
+Energy (ENERGY) – Risorsa consumata per abilità e azioni speciali.
+
+Stati
+
+Exhausted (NRG < 10%) : Riduzione del 50% di AGI e STR.
+Berserk (HP < 25%) : +25% STR, ma -20% DEF e impossibilità di usare abilità tattiche.
+Fatica (RES < 20%) : Riduzione della velocità di turno del 30%.
+Inspired (INT > 80) : Le abilità costano il 25% in meno di NRG.
+Sneaky (AGI > 90) : Aumenta la probabilità di schivata del 40% fino all'attacco successivo (si attiva all’inizio del turno).
+
+Azioni Disponibili
+
+Idle : Il personaggio è fermo, in attesa di un'azione.
+Attacking : Il personaggio sta attaccando un bersaglio.
+Casting : Il personaggio sta lanciando un incantesimo o un'abilità speciale.
+Moving : Il personaggio sta cambiando posizione sul campo di battaglia.
+Using Item : Il personaggio sta usando un oggetto (pozione, cibo, ecc.).
+
+Azioni di combattimento
+
+Assault*( Basato su STR)*
+Attacco che infligge il 150% del danno base ma ha una velocità ridotta (-30% AGI).
+Dodge*(Basato su AGI)*
+Aumenta la probabilità di schivata del 50% fino al turno successivo.
+Meditation (Basato su INT)
+Recupera il 30% di NRG e aumenta l’INT di 10 punti temporaneamente.
+Guard (Basato su RES)
+Diminuisce il danno subito del 50% per un turno.
+Stealth Shot (Basato su AGI e STR)
+Se il personaggio è in stato Sneaky, infligge un colpo critico automatico con danno raddoppiato.
+
+
