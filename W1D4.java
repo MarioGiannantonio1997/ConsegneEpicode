41a487293e574e305e96564ee4833fa9f6bdbe81
Uro <93337886+MarioGiannantonio1997@users.noreply.github.com>
2025-04-10 19:31:47 +0200

    Add files via upload

---
+++ W1D4.java
@@ -0,0 +1,12 @@
+Gioco: Exploding Kittens
+Regola: Doppio Tiro della Sorte
+Quando un giocatore pesca un Exploding Kitten, invece di esplodere automaticamente, tira due dadi a 6 facce. La somma dei due dadi determina l’effetto :
+
+Somma dei Dadi	Effetto 
+1 - 4	Esplosione immediata, nessuna possibilità di difesa!
+5 - 6	Il gatto esplode, ma il giocatore deve scartare una carta e darla a un altro giocatore
+7 - 9	Il giocatore evita l’esplosione ma prossimo turno é doppio 
+10	Il giocatore rimette il Kitten nel mazzo e poi mescola
+11	 Il giocatore cambia l'Exploding Kitten con una carta a caso da un altro giocatore!
+12	 Il gatto diventa temporaneamente amico del giocatore, proteggendolo dal prossimo attacco.
+
